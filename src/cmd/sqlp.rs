b35aec7c04b59d02576d53086d33ffd5f104f8f3
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-04-01 08:04:13 -0400

    docs: `sqlp` expanded `sqlp` description in usage text
    [skip ci]

--- src/cmd/sqlp.rs
+++ src/cmd/sqlp.rs
@@ -1,9 +1,10 @@
 static USAGE: &str = r#"
 Run blazing-fast Polars SQL queries against several CSVs - replete with joins, aggregations,
-grouping, table functions, sorting, and more - working on larger than memory CSV files.
+grouping, table functions, sorting, and more - working on larger than memory CSV files directly,
+without having to load it first into a database.
 
-Polars SQL is a SQL dialect, converting SQL queries to fast Polars LazyFrame expressions
-(see https://docs.pola.rs/user-guide/sql/intro/).
+Polars SQL is a SQL dialect (modelled after PostgreSQL), converting SQL queries to ultra-fast
+Polars LazyFrame expressions (see https://docs.pola.rs/user-guide/sql/intro/).
 
 For a list of SQL functions and keywords supported by Polars SQL, see
 https://docs.pola.rs/py-polars/html/reference/sql/index.html though be aware that it's for
