8da75a9fa4c2455a39636057675f9d93674c2998
rzmk <30333942+rzmk@users.noreply.github.com>
2025-04-21 10:53:47 -0400

    feat: --latitude and --longitude for geoconvert

--- src/cmd/geoconvert.rs
+++ src/cmd/geoconvert.rs
@@ -13,6 +13,10 @@ To convert a CSV file into GeoJSON data, specify the WKT geometry column with th
 
 qsv geoconvert file.csv csv geojson --geometry geometry
 
+Alternatively specify the latitude and longitude columns with the --latitude and --longitude flags:
+
+qsv geoconvert file.csv csv geojson --latitude lat --longitude lon
+
 Usage:
     qsv geoconvert [options] (<input>) (<input-format>) (<output-format>)
     qsv geoconvert --help
@@ -24,11 +28,14 @@ geoconvert REQUIRED arguments:
     <output-format>   Valid values are:
                       - For GeoJSON input: "csv", "svg", and "geojsonl"
                       - For SHP input: "csv", "geojson", and "geojsonl"
-                      - For CSV input: "geojson", "geojsonl", and "svg"
+                      - For CSV input: "geojson", "geojsonl", "csv", and "svg"
 
 geoconvert options:
-                                 REQUIRED FOR CSV INPUT:
+                                 REQUIRED FOR CSV INPUT
     -g, --geometry <geometry>    The name of the column that has WKT geometry.
+                                 Alternative to --latitude and --longitude.
+    -y, --latitude <col>         The name of the column with northing values.
+    -x, --longitude <col>        The name of the column with easting values.
 
 Common options:
     -h, --help                   Display this message
@@ -73,11 +80,13 @@ enum OutputFormat {
 
 #[derive(Deserialize)]
 struct Args {
-    arg_input:         Option<String>,
-    arg_input_format:  InputFormat,
+    arg_input: Option<String>,
+    arg_input_format: InputFormat,
     arg_output_format: OutputFormat,
-    flag_geometry:     Option<String>,
-    flag_output:       Option<String>,
+    flag_latitude: Option<String>,
+    flag_longitude: Option<String>,
+    flag_geometry: Option<String>,
+    flag_output: Option<String>,
 }
 
 impl From<geozero::error::GeozeroError> for CliError {
@@ -211,6 +220,9 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
             wtr.write_all(output_string.as_bytes())?;
         },
         InputFormat::Csv => {
+            if args.flag_geometry.is_some() && (args.flag_latitude.is_some() || args.flag_longitude.is_some()) {
+                return fail_clierror!("Cannot use --geometry flag with --latitude or --longitude.");
+            }
             if let Some(geometry_col) = args.flag_geometry {
                 let mut csv = geozero::csv::CsvReader::new(&geometry_col, buf_reader);
                 match args.arg_output_format {
@@ -231,8 +243,68 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                     },
                 }
             } else {
+                if let Some(y_col) = args.flag_latitude {
+                    if let Some(x_col) = args.flag_longitude {
+                        let mut rdr = csv::Reader::from_reader(buf_reader);
+                        let headers = rdr.headers()?.clone();
+                        let mut feature_collection = serde_json::json!({"type": "FeatureCollection", "features": []});
+                        
+                        for result in rdr.records() {
+                            let record = result?;
+                            let mut feature = serde_json::json!({"type": "Feature", "geometry": {}, "properties": {}});
+
+                            // Add lat/lon coordinates geometry
+                            let latitude_col_index = headers.iter().position(|y| y == y_col).unwrap();
+                            let longitude_col_index = headers.iter().position(|x| x == x_col).unwrap();
+                            let latitude_value = record.get(latitude_col_index).unwrap().parse::<f64>().unwrap();
+                            let longitude_value = record.get(longitude_col_index).unwrap().parse::<f64>().unwrap();
+                            let geometry = feature.get_mut("geometry").unwrap();
+                            let geometry_obj = geometry.as_object_mut().unwrap();
+                            geometry_obj.insert("type".to_string(), serde_json::Value::from("Point"));
+                            geometry_obj.insert("coordinates".to_string(), serde_json::Value::from(vec![latitude_value, longitude_value]));
+                            
+                            // Add properties
+                            for (index, value) in record.iter().enumerate() {
+                                if index != longitude_col_index && index != latitude_col_index {
+                                    let properties = feature.get_mut("properties").unwrap();
+                                    let properties_obj = properties.as_object_mut().unwrap();
+                                    let new_key = headers.get(index).unwrap().to_string();
+                                    let new_value = serde_json::Value::from(value);
+                                    properties_obj.insert(new_key, new_value);
+                                }
+                            }
+
+                            // Add Feature to FeatureCollection
+                            let features = feature_collection.get_mut("features").unwrap();
+                            let features_array = features.as_array_mut().unwrap();
+                            features_array.push(feature);
+                        }
+
+                        // Write FeatureCollection
+                        let fc_string = feature_collection.to_string();
+                        let mut geometry = geozero::geojson::GeoJson(&fc_string);
+                        match args.arg_output_format {
+                            OutputFormat::Csv => {
+                                let mut processor = CsvWriter::new(&mut wtr);
+                                geometry.process(&mut processor)?;
+                            },
+                            OutputFormat::Svg => {
+                                let mut processor = SvgWriter::new(&mut wtr, false);
+                                geometry.process(&mut processor)?;
+                            },
+                            OutputFormat::Geojsonl => {
+                                let mut processor = GeoJsonLineWriter::new(&mut wtr);
+                                geometry.process(&mut processor)?;
+                            },
+                            OutputFormat::Geojson => {
+                                wtr.write_all(fc_string.as_bytes())?;
+                            },
+                        }
+                        return Ok(wtr.flush()?);
+                    }
+                }
                 return fail_clierror!(
-                    "Please specify a geometry column with the --geometry option"
+                    "Please specify a geometry column with the --geometry option or longitude/latitude with the --latitude and --longitude options."
                 );
             }
         },
