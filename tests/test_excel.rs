0b314ac7393e5fdde6c39d8b4393865746e1bb26
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-07-04 08:47:51 -0400

    Revert "tests: `excel` re-enable skipped formula tests"
    This reverts commit 2d31b8a56f8e5f7d612495aaa953a543f92ec346.
    
    `excel` formula tests work on dev, but on GitHub Actions Test...

--- tests/test_excel.rs
+++ tests/test_excel.rs
@@ -45,7 +45,7 @@ fn excel_cellerrors() {
 }
 
 #[test]
-// #[ignore = "skip this for now as its flaky retrieving the formula defn"]
+#[ignore = "skip this for now as its flaky retrieving the formula defn"]
 fn excel_cellerrors_formula() {
     let wrk = Workdir::new("excel_cellerrors_formula");
 
@@ -77,7 +77,7 @@ fn excel_cellerrors_formula() {
 }
 
 #[test]
-// #[ignore = "skip this for now as its flaky retrieving the formula defn"]
+#[ignore = "skip this for now as its flaky retrieving the formula defn"]
 fn excel_cellerrors_both() {
     let wrk = Workdir::new("excel_cellerrors_both");
 
