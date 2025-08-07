35a906385afa64c82be4200f38f8c96af8624e4d
Jason Nguyen <dat.nguyen@bitbytelab.io>
2025-08-07 14:46:15 +0700

    feat: add initial implementation of the qsv library

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+extern crate qsv_docopt as docopt;
+
+pub use clitypes::{CURRENT_COMMAND, CliError, CliResult, QsvExitCode};
+pub use config::SPONSOR_MESSAGE;
+pub use docopt::Docopt;
+use rand::Rng;
+use serde::Deserialize;
+
+#[cfg(feature = "jemallocator")]
+#[global_allocator]
+pub static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+pub static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+pub mod clitypes;
+pub mod cmd;
+pub mod config;
+pub mod index;
+pub mod lookup;
+pub mod odhtcache;
+pub mod select;
+pub mod util;
