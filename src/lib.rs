f10f9064048baa12557ec143670fcd2a7a03350f
Jason Nguyen <dat.nguyen@bitbytelab.io>
2025-08-07 16:37:33 +0700

    refactor: remove commented-out global allocator configurations

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,6 @@ pub use clitypes::{CURRENT_COMMAND, CliError, CliResult, QsvExitCode};
 pub use config::SPONSOR_MESSAGE;
 pub use docopt::Docopt;
 
-// #[cfg(feature = "jemallocator")]
-// #[global_allocator]
-// pub static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-// #[cfg(feature = "mimalloc")]
-// #[global_allocator]
-// pub static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 pub mod clitypes;
 pub mod cmd;
 pub mod config;
